26b57ba3812700ecff932d13cf9a5ac51f08faaf
ledguty <ledguty@gmail.com>
2024-06-21 20:37:03 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@
 sudo apt-get update
 sudo apt-get install -y tesseract-ocr
 sudo apt-get install -y tesseract-ocr-spa  # Para soporte en español
+
+# Verificar la instalación
+tesseract --version
+
+# Crear un enlace simbólico si es necesario
+sudo ln -s /usr/bin/tesseract /usr/local/bin/tesseract
